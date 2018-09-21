8e287c02c0682f5bd7d3f998df25d423e37b3649
Gezili <zilig1998@gmail.com>
2018-09-21 15:59:04 -0400

    unique providers
    code and csv for unique provider list

--- import_files.py
+++ import_files.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas
 from pathlib import Path
+import numpy as np
 
 directory = str(Path(__file__).parents[0])
 sys.path.append(directory)
@@ -38,14 +39,43 @@ def build_list_costs():
     for i, id in enumerate(costs):
         cost_arr[procedure_list_unique.index(procedure_list[i])].append(id)
 
+    '''
     with open(directory + r'\procedure_cost.csv', 'a') as f:
         for i, elem in enumerate(cost_arr):
             f.write(str(procedure_list_unique[i]) + ',')
             for cost in elem:
                 f.write(str(cost) + ',')
             f.write('\n')
+    '''
+    list_mean = []
+    list_std = []
+    num_elements = []
+    
+    for elem in cost_arr:
+        list_mean.append(np.mean(elem))
+        list_std.append(np.std(elem))
+        num_elements.append(len(elem))
+        
+    with open(directory + r'\procedure_stats.csv', 'a') as f:
+        for i in range(len(procedure_list_unique)):
+            f.write(str(procedure_list_unique[i]) + ',')
+            f.write(str(list_mean[i]) + ',')
+            f.write(str(list_std[i]) + ',')
+            f.write(str(num_elements[i]) + '\n')
             
-    return procedure_list_unique, cost_arr
-            
+def build_provider_info():
+    
+    list = pandas.read_csv(directory + r'/claims_final.csv')
+    provider_list = list.ix[:,2]
+    provider_list_unique = []
+    for i, id in enumerate(provider_list):
+        if id not in provider_list_unique:
+            provider_list_unique.append(id)
+    
+    with open(directory + r'\unique_providers.csv', 'a') as f:        
+        for id in provider_list_unique:
+           f.write(str(id) + ',\n')
+    
+    return provider_list_unique
     
     
